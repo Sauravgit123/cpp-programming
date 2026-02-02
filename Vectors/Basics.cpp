0040ae925e48b696d9441d421558e57df523dd20
Saurav <saurav316kumar@gmail.com>
2026-02-02 21:02:03 +0530

    dsa day-1-vectors-basicsintro

---
+++ Vectors/Basics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // dynamic arrays (can be resize..alter the size of arrays)  contiguous m/m allocation
+
+    vector<int> v1; //   OR vector <int> v1(size);
+                    // v.size()--> return the length of the vector
+                    // v.resize(new size)..change the size of vector
+    return 0;
+}
