e03b16a6015d320301295ec293d737808bab0e17
Saurav <saurav316kumar@gmail.com>
2026-02-06 20:43:30 +0530

    day-5-dsa_array-ques added

--- Arrays/DSA-DAY-5.cpp
+++ Arrays/DSA-DAY-5.cpp
@@ -5,6 +5,36 @@ int main()
 {
     // PREFIX SUM - RETURN THE RUNNING SUM OF THE GIVEN ARRAY
 
+    // int n;
+    // cout << "Enter the no. of elements in array : ";
+    // cin >> n;
+    // int arr[n];
+    // cout << "Enter the elements : ";
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+    // cout << endl
+    //      << "Given array : ";
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // for (int i = 1; i < n; i++)
+    // {
+    //     arr[i] = arr[i] + arr[i - 1];
+    // }
+    // cout << endl
+    //      << "Output array : ";
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // divide the arra yinto 2 where 2 subarrays sum is equal to each other
+    // prefix sum == suffix sum
+
     int n;
     cout << "Enter the no. of elements in array : ";
     cin >> n;
@@ -20,16 +50,29 @@ int main()
     {
         cout << arr[i] << " ";
     }
-
-    for (int i = 1; i < n; i++)
-    {
-        arr[i] = arr[i] + arr[i - 1];
-    }
-    cout << endl
-         << "Output array : ";
+    int tsum = 0;
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i] << " ";
+        tsum += arr[i];
+    }
+    int psum = 0;
+    int f = 0;
+    for (int i = 0; i < n; i++)
+    {
+        psum += arr[i];
+        int s_sum = tsum - psum;
+        if (s_sum == psum)
+        {
+            cout << endl
+                 << "True";
+            f = 1;
+            break;
+        }
+    }
+    if (f == 0)
+    {
+        cout << endl
+             << "Not found";
     }
 
     return 0;
