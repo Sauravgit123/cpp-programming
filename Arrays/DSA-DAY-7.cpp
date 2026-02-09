a3c7cb630b254e4db4f0cb3307d651d2401156b4
Saurav <saurav316kumar@gmail.com>
2026-02-09 20:48:41 +0530

    2pointer_array ques

--- Arrays/DSA-DAY-7.cpp
+++ Arrays/DSA-DAY-7.cpp
@@ -5,6 +5,40 @@ int main()
 {
     // There is a array of size n sorted in inc order ,find the pair of elements in the array whose sum =x
 
+    // int arr[5] = {-1, 0, 1, 2, 3};
+    // cout << "Given array :";
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+    // int x;
+    // cout << endl
+    //      << "Enter the target sum x :";
+    // cin >> x;
+    // int f = -1;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int j = i + 1; j < 5; j++)
+    //     {
+    //         if (arr[i] + arr[j] == x)
+    //         {
+    //             cout << "Yes";
+    //             f = 1;
+    //             break;
+    //         }
+    //     }
+    //     if (f == 1)
+
+    //         break;
+    // }
+    // if (f == -1)
+    // {
+    //     cout << "No";
+    // }
+
+    // NOW BY 2 POINTER APPROCH;
+
     int arr[5] = {-1, 0, 1, 2, 3};
     cout << "Given array :";
 
@@ -16,40 +50,35 @@ int main()
     cout << endl
          << "Enter the target sum x :";
     cin >> x;
-    int f = -1;
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = i + 1; j < 5; j++)
-        {
-            if (arr[i] + arr[j] == x)
-            {
-                cout << "Yes";
-                f = 1;
-                break;
-            }
-        }
-        if (f == 1)
 
-            break;
+    int i = 0;     // Left pointer (start)
+    int j = 5 - 1; // Right pointer (end)
+    int f = -1;
+
+    while (i < j)
+    {
+        int currentSum = arr[i] + arr[j];
+
+        if (currentSum == x)
+        {
+            cout << "Yes";
+            f = 1;
+            break; // Pair mil gaya, loop se bahar
+        }
+        else if (currentSum < x)
+        {
+            i++; // Sum chota hai, toh left pointer badhao
+        }
+        else
+        {
+            j--; // Sum bada hai, toh right pointer kam karo
+        }
     }
+
     if (f == -1)
     {
         cout << "No";
     }
-
-    // NOW BY 2 POINTER APPROCH;
-
-    nt arr[5] = {-1, 0, 1, 2, 3};
-    cout << "Given array :";
-
-    for (int i = 0; i < 5; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    int x;
-    cout << endl
-         << "Enter the target sum x :";
-    cin >> x;
 
     return 0;
 }
