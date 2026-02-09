7fdee7e1bdae0832c50db268768bcf61246becee
Saurav <saurav316kumar@gmail.com>
2026-02-09 20:36:57 +0530

    question_review

--- Arrays/DSA-DAY-7.cpp
+++ Arrays/DSA-DAY-7.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 int main()
 {
-    //
+    // There is a array of size n sorted in inc order ,find the pair of elements in the array whose sum =x
+
+    int arr[5] = {-1, 0, 1, 2, 3};
+    cout << "Given array :" << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i + 1; j < 5; j++)
+        {
+            if
+        }
+    }
 
     return 0;
 }
