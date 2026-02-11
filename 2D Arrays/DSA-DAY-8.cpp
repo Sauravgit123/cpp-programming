fd9c526cea9e717392fd206dcab70236bd523dca
Saurav <saurav316kumar@gmail.com>
2026-02-11 20:47:06 +0530

    2-D array - revision

---
+++ 2D Arrays/DSA-DAY-8.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cout << "Enter the no. of rows : ";
+    cin >> m;
+    cout << "Enter the no. of columns : ";
+    cin >> n;
+
+    int arr[m][n];
+    int arr2[m][n];
+    cout << endl
+         << "Enter the elements for 1st matrix : " << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    cout << endl
+         << "Enter the elements for 2nd matrix : " << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr2[i][j];
+        }
+    }
+    cout << endl
+         << "Given matrixes : " << endl
+         << "A-matrix" << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "B-matrix" << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr2[i][j] << " ";
+        }
+        cout << endl;
+    }
+    int ans[m][n];
+
+    return 0;
+}
